6bba7c55f3427b655d5c6f132aa69a6018d09cda
MaciejLacky <57499330+MaciejLacky@users.noreply.github.com>
2021-05-08 11:09:31 +0200

    Add project files.

---
+++ RegisterAndLoginApp/Controllers/LoginController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RegisterAndLoginApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RegisterAndLoginApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ProcessLogin(UserModel userModel)
+        {
+            if (userModel.UserName == "BillGates" && userModel.Password == "bigbucks")
+            {
+                return View("LoginSucces", userModel);
+            }
+            else
+            {
+                return View("LoginFailure", userModel);
+            }
+            
+        }
+    }
+}
---
+++ RegisterAndLoginApp/Services/SecurityService.cs
@@ -0,0 +1,13 @@
+﻿using RegisterAndLoginApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RegisterAndLoginApp.Services
+{
+    public class SecurityService
+    {
+        List<UserModel> knownUsers = new List<UserModel>();
+    }
+}
